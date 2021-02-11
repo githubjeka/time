cc3efcd4aace3b70c5c71d8733edfe5440f305a7
githubjeka <et.coder@gmail.com>
2021-02-11 16:14:48 +0300

    feat: Ability to split period by time points

--- src/etcoder/Time/Calculations/Calculator.php
+++ src/etcoder/Time/Calculations/Calculator.php
@@ -15,21 +15,36 @@ class Calculator
     {
         $result = [];
 
+        usort(
+            $instants,
+            function (Instant $a, Instant $b) {
+                if ($a->compareTo($b)->isEqual()) {
+                    return 0;
+                }
+
+                if ($a->compareTo($b)->isLess()) {
+                    return -1;
+                }
+                return 1;
+            }
+        );
+
+        $start = $period->secondScale()->start();
+
         foreach ($instants as $instant) {
             if ($period->relativeTo($instant)->isBetween()) {
-                $result[] = new Period(
-                    $period->secondScale()->start(),
-                    $instant
-                );
-                $result[] = new Period(
-                    $instant,
-                    $period->secondScale()->end()
-                );
+                $result[] = new Period($start, $instant);
+                $start = $instant;
             }
         }
 
         if (count($result) === 0) {
             return new Periods($period);
+        } else {
+            $result[] = new Period(
+                $start,
+                $period->secondScale()->end()
+            );
         }
 
         return new Periods(...$result);
